98275a6a3f46fe0c8cb3cfd81aa2c5b24204bb5e
Xing Wang <cabbit007x@gmail.com>
2021-10-22 16:08:05 -0500

    Cleanup packages

--- src/web/lib/validator.js
+++ src/web/lib/validator.js
@@ -1,9 +1,11 @@
-export default (options) => ({ schema }) => (data) => {
-  // with option strict = false, yup `validateSync` function returns the coerced value if validation was successful, or throws if validation failed
-  try {
-    const result = schema.validateSync(data, options);
-    return { value: result };
-  } catch (error) {
-    return { error };
-  }
-};
+export default (options) =>
+  ({ schema }) =>
+  (data) => {
+    // with option strict = false, yup `validateSync` function returns the coerced value if validation was successful, or throws if validation failed
+    try {
+      const result = schema.validateSync(data, options);
+      return { value: result };
+    } catch (error) {
+      return { error };
+    }
+  };
